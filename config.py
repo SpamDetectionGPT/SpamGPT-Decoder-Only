ea239f09fc707106274b8d31329d972122933caa
Raz200327 <115984448+Raz200327@users.noreply.github.com>
2025-04-13 21:53:47 +1000

    Tokeniser setup/inference functionality

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ from dataclasses import dataclass
 
 @dataclass
 class SpamGPTConfig:
+    latent_dim_kv = 512
     block_size = 256
-    vocab_size = 50262
+    vocab_size = 50263
     n_layer = 12
     n_head = 12
     n_emb = 768
--- spamGPT.py
+++ spamGPT.py
@@ -3,6 +3,24 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import math
+import tiktoken
+
+base_encoding = tiktoken.get_encoding("gpt2")
+special_tokens = {
+    "<SOE>": 50257,  # Add one after the vocab size (GPT-2 vocab size is 50257)
+    "<EOE>": 50258,
+    "<SOP>": 50259,
+    "<EOP>": 50260,
+    "<SPAM>": 50261,
+    "<HAM>": 50262,
+}
+enc = tiktoken.Encoding(
+    name="gpt2-custom",
+    pat_str=base_encoding._pat_str,
+    mergeable_ranks=base_encoding._mergeable_ranks,
+    special_tokens={**base_encoding._special_tokens, **special_tokens}
+)
+
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
@@ -80,11 +98,28 @@ class SpamGPT(nn.Module):
 
         x = self.transformer['ln_f'](x)
         return self.lm_head(x)
-    
+
+
+def run_inference(input_tokens, max_length, model, temp, enc, endtoken=None):
+    model.eval()
+    print("\n\n-----------------Inference-----------------\n")
+    while input_tokens.size()[1] < max_length:
+        if endtoken is not None and input_tokens[0, -1].item() == endtoken:
+            break
+        pred = model(input_tokens)
+        logits = pred[:,-1, :]
+        logits = logits/temp
+        probs = F.softmax(logits, dim=-1)
+        next_token = torch.multinomial(probs, num_samples=1)
+        print(enc.decode([next_token.squeeze().tolist()]), end="")
+        input_tokens = torch.cat((input_tokens, next_token), dim=1)
+    print("\n\n---------------End Inference---------------\n")
+    return input_tokens
+
 
 if __name__ == "__main__":
     #example use
     model = SpamGPT(SpamGPTConfig)
-    x = torch.tensor([[1, 4, 9]], dtype=torch.long)
-    print(model(x))
+    x = torch.tensor([[50257, 4, 9]], dtype=torch.long)
+    run_inference(x, 20, model, 0.7, enc)
     
